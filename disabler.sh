91a6141ae90d0f4c88fa0d47d5341237dc565a6f
Rodrigo Matos <rodrigo.matos9@hotmail.com>
2020-03-31 13:32:06 -0300

    Add files via upload

--- disabler.sh
+++ disabler.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Disable Dell Inspiron N4050 touchpad and internal keyboard
+# Disables Dell Inspiron N4050 touchpad and internal keyboard
 
 xinput disable "AlpsPS/2 ALPS GlidePoint"
 xinput disable "AT Translated Set 2 keyboard"
-echo disable touchpad and internal keyboard disabled successfully.
+echo touchpad and internal keyboard disabled successfully.
