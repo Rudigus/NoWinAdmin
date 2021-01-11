6797c1564ead362f9dee75b169c1a5c11ee7fe9b
Rodrigo Matos <rodrigo.matos9@hotmail.com>
2021-01-11 19:57:29 -0300

    Update audiotovideo.sh

--- audiotovideo.sh
+++ audiotovideo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This is a script to convert audio to video (with a still image) using ffmpeg
+# Dependencies: ffmpeg, exiftool
 
 echo "Audio file(s): "
 read audio
@@ -8,5 +9,11 @@ read image
 for track in $audio; do 
 	filename="${track##*/}"
 	filename="${filename%.*}"
+	title="$(exiftool -s -s -s -title $track)"
+	artist="$(exiftool -s -s -s -artist $track)" 
+	if [ -n "$title" ] && [ -n "$artist" ]
+	then
+		filename="$title - $artist"
+	fi
 	ffmpeg -loop 1 -i $image -i $track -shortest -f mp4 "$filename.mp4"
 done
