b1d2602d964f0c367803e1c8ca6b7be0d90cc0fc
Rodrigo Matos <rodrigo.matos9@hotmail.com>
2021-01-11 15:28:16 -0300

    Add files via upload

---
+++ audiotovideo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This is a script to convert audio to video (with a still image)
+
+echo "Audio file(s): "
+read audio
+echo "Image file: "
+read image
+for track in $audio; do 
+	filename="${track##*/}"
+	filename="${filename%.*}"
+	ffmpeg -loop 1 -i $image -i $track -shortest -f mp4 "$filename.mp4"
+done
