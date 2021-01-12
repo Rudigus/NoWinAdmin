8abe25fb68e85127dd50580f1bc1a229e3917eb4
Rodrigo Matos <rodrigo.matos9@hotmail.com>
2021-01-12 17:32:09 -0300

    Minor improvements
    Added comments to help for future reference and fixed a problem which happened when the track had a question mark (?) in its title metadata. Since question mark is forbidden in Windows filenames, the track wouldn't convert. Now it changes the question mark for a underscore (_)

--- audiotovideo.sh
+++ audiotovideo.sh
@@ -6,14 +6,22 @@ echo "Audio file(s): "
 read audio
 echo "Image file: "
 read image
+# Supports converting multiple tracks
 for track in $audio; do 
+	# Removes prefix and extension from given path
 	filename="${track##*/}"
 	filename="${filename%.*}"
+	# Title Metadata
 	title="$(exiftool -s -s -s -title $track)"
-	artist="$(exiftool -s -s -s -artist $track)" 
+	# Artist Metadata
+	artist="$(exiftool -s -s -s -artist $track)"
+	# If possible, have a nice track filename
 	if [ -n "$title" ] && [ -n "$artist" ]
 	then
 		filename="$title - $artist"
 	fi
+	# Removes unwanted characters and replace them with underscore
+	filename="${filename//'?'/_}"
+	# Where the magic happens
 	ffmpeg -loop 1 -i $image -i $track -shortest -f mp4 "$filename.mp4"
 done
